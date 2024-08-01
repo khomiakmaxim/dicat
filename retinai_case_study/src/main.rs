2d4e8c3aad9dc83cb4a9498500b79129076bc369
Maksym Khomiak <khomiakmaxim@gmail.com>
2024-08-01 16:06:58 +0300

    Implemented simplest PoC

--- retinai_case_study/src/main.rs
+++ retinai_case_study/src/main.rs
@@ -1,15 +1,41 @@
+use std::borrow::Cow;
+
 use dicom::{dictionary_std::tags, object::open_file};
+use walkdir::WalkDir;
+
+// use async_walkdir::WalkDir;
+// use futures_lite::future::block_on;
+// use futures_lite::stream::StreamExt;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let obj = open_file("test_dicom_files/0002.DCM")?;
+    for entry in WalkDir::new("test_dicom_files") {
+        let entry = entry.unwrap();
 
-    let patient_name = obj.element(tags::PATIENT_NAME)?.to_str()?;
-    let patient_clinic = obj.element(tags::INSTITUTION_NAME)?.to_str()?;
-    let patient_id = obj.element(tags::PATIENT_ID)?.to_str()?;
+        if entry.file_type().is_dir() {
+            continue;
+        }
 
-    dbg!(patient_name);
-    dbg!(patient_clinic);
-    dbg!(patient_id);
+        let path = entry.path();
+
+        let Ok(obj) = open_file(path) else {
+            continue;
+        };
+
+        let file_name = path.as_os_str().to_str().unwrap();
+
+        let patient_name = obj.element(tags::PATIENT_NAME)?.to_str()?;
+        let patient_clinic = match obj.element(tags::INSTITUTION_NAME) {
+            Ok(inner) => inner.to_str()?,
+            Err(_) => Cow::Borrowed("No clinic name"),
+        };
+        let patient_id = obj.element(tags::PATIENT_ID)?.to_str()?;
+
+        println!("------------------------------------------");
+        println!(
+            "{}, {}, {}, {}",
+            file_name, patient_name, patient_clinic, patient_id
+        );
+    }
 
     Ok(())
 }
